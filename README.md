3ef6e388deaee0c66875310de301cd06e683e6f7
Matteo Collina <hello@matteocollina.com>
2015-02-03 00:27:54 +0100

    Initial import.

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # docker-loghose
+
 Collect all the logs from all docker containers
+
+## Install
+
+As a command line tool:
+
+```bash
+npm install docker-loghose -g
+```
+
+Embedded usage
+
+```bash
+npm install docker-loghose --save
+```
+
+## Embedded Usage
+
+```
+var loghose = require('loghose')
+loghose.pipe(process.stdout)
+```
+
+## Data format
+
+```js
+{
+  v: 0,
+  id: "3324acd73ad5ed7aa5d35675fd3e5f34d8a3ee4ea77c19239cfa113e47d0ddce",
+  image: "mosca:latest"
+}
+```
+
+## Command Line Usage
+
+```bash
+docker-loghose
+```
+
+Acknowledgements
+----------------
+
+This project was kindly sponsored by [nearForm](http://nearform.com).
+
+
+## License
+
+MIT
---
+++ loghose.js
@@ -0,0 +1,79 @@
+#! /usr/bin/env node
+
+var nes = require('never-ending-stream')
+var through = require('through2')
+var split = require('split2')
+var Docker = require('dockerode')
+var pump = require('pump')
+
+function logHose (opts) {
+  var docker = new Docker(opts)
+  var result = through.obj()
+  var events = nes(function(cb) {
+        docker.getEvents(cb)
+      })
+  var streams = {}
+  var oldDestroy = result.destroy
+
+  result.destroy = function() {
+    Object.keys(streams).forEach(function(stream) {
+      stream.destroy()
+    })
+    oldDestroy.call(this)
+  }
+
+  events.pipe(through(function(chunk, enc, cb) {
+    var data = JSON.parse(chunk)
+    if (data.status === 'start') {
+      data.Id = data.id
+      data.Image = data.from
+      attachContainer(data)
+    } else if (data.status === 'stop' || data.status === 'die') {
+      if (streams[data.id]) {
+        streams[data.id].destroy()
+        delete streams[data.id]
+      }
+    }
+    cb()
+  }))
+
+  docker.listContainers(function(err, containers) {
+    if (err) {
+      return result.emit('error', err);
+    }
+    containers.forEach(attachContainer);
+  });
+
+  return result
+
+  function attachContainer(data) {
+    var container = docker.getContainer(data.Id);
+    var stream = nes(function(cb) {
+          container.attach({stream: true, stdout: true, stderr: true}, cb)
+        })
+    streams[data.Id] = stream
+    pump(
+      stream,
+      split(),
+      through.obj(function(chunk, enc, cb) {
+        this.push({
+          v: 0,
+          id: data.Id,
+          image: data.Image,
+          line: chunk
+        })
+        cb()
+      }))
+      .pipe(result, { end: false })
+  }
+}
+
+module.exports = logHose
+
+if (require.main === module) {
+  logHose().pipe(through.obj(function(chunk, enc, cb) {
+    this.push(JSON.stringify(chunk))
+    this.push('\n')
+    cb()
+  })).pipe(process.stdout)
+}
